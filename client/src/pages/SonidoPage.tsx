075046571ec85e41d4ebe92b0e3eddbf33a020f2
Juan Genero <juangenesp@gmail.com>
2025-06-17 22:16:28 +0200

    quitar img soundboard

--- client/src/pages/SonidoPage.tsx
+++ client/src/pages/SonidoPage.tsx
@@ -1,23 +1,13 @@
 import BaseLayout from '@/layouts/BaseLayout';
-import { title } from '@/shared/components/primitives';
-import { useTheme } from '@heroui/use-theme';
+import { subtitle, title } from '@/shared/components/primitives';
 
 export default function SonidoPage() {
-  const { theme } = useTheme();
-
   return (
     <BaseLayout>
       <h1 className={title()}>Sonidos</h1>
 
-      <div>
-        <img
-          src={theme === 'light' ? '/images/soundboard_light.png' : '/images/soundboard_dark.png'}
-          alt="soundboard"
-          onClick={() => {
-            window.open('https://alcala-city.github.io/soundboard', '_blank');
-          }}
-          className="rounded-lg cursor-pointer mt-10"
-        />
+      <div className="mt-5">
+        <h1 className={subtitle()}>Sonidisimo</h1>
       </div>
     </BaseLayout>
   );
